92ac2053ad85c46f91c330838b04778cd480d1ac
nho1ix <tupilbewb@gmail.com>
2020-09-20 11:47:51 -0700

    changed from monospace to Product Sans, and tried making emojis show up

--- config.h
+++ config.h
@@ -3,9 +3,9 @@
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
-static const char *fonts[] = {
-	"monospace:size=10"
-};
+static const char *fonts[] = { "Product Sans:size=11",
+                               "NotoColorEmoji:size=11" };
+
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
