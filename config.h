fbf7568ea63cccd53893cef4dd32348d7c97e65b
nho1ix <tupilbewb@gmail.com>
2020-09-20 18:46:07 -0700

    changed font size + colors

--- config.h
+++ config.h
@@ -3,14 +3,15 @@
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
-static const char *fonts[] = { "Product Sans:size=11",
+static const char *fonts[] = { "Product Sans:size=10",
                                "NotoColorEmoji:size=11" };
 
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#bbbbbb", "#000000" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
+//	[SchemeSel] = { "#eeeeee", "#005577" },
+	[SchemeSel] = { "#FFFFFF", "#575b7f" },
 	[SchemeOut] = { "#bbbbbb", "#000000" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
