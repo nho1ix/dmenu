31b653ec24107949b399a22f2896c05f7f9e8d4f
nho1ix <tupilbewb@gmail.com>
2020-09-21 22:50:40 -0700

    bump font size from 15 to 17

--- config.h
+++ config.h
@@ -5,7 +5,7 @@ static int topbar = 1;                      /* -b  option; if 0, dmenu appears a
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = { "Product Sans:size=10.5",
                                "font-awesome:pixelsize=10:antialias=true:autohint=true",
-                               "NotoColorEmoji:pixelsize=15" };
+                               "NotoColorEmoji:pixelsize=17" };
 
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
