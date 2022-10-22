a9b8e766fff56317dd5961d42f8b826bd1981c38
Victor Vo <tupilbewb@gmail.com>
2022-10-22 01:00:43 -0700

    Change font from Product Sans to Overpass

--- config.h
+++ config.h
@@ -3,7 +3,7 @@
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
-static const char *fonts[] = { "Product Sans:size=12:antialias=true:autohint=true",
+static const char *fonts[] = { "Overpass:style=Regular:size=13:antialias=true:autohint=true",
                                "font-awesome:pixelsize=10:antialias=true:autohint=true",
                                "NotoColorEmoji:pixelsize=17" };
 
