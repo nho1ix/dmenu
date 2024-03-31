a49ae2f49b82d1170bccaf4706930ea0009cec78
Victor Vo <fromvictorvo@gmail.com>
2024-03-31 01:59:55 -0700

    Current config settings used

--- config.h
+++ config.h
@@ -3,7 +3,7 @@
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
-static const char *fonts[] = { "Overpass:style=Regular:size=11:antialias=true:autohint=true",
+static const char *fonts[] = { "Overpass:style=Regular:size=13:antialias=true:autohint=true",
                                "font-awesome:pixelsize=10:antialias=true:autohint=true",
                                "NotoColorEmoji:pixelsize=44" };
 
@@ -13,7 +13,8 @@ static char *prompt      = NULL;      /* -p  option; prompt to the left of input
 static char normfgcolor[] = "#bbbbbb";
 static char normbgcolor[] = "#222222";
 static char selfgcolor[]  = "#eeeeee";
-static char selbgcolor[]  = "#414350";
+static char selbgcolor[]  = "#b55617";
+// static char selbgcolor[]  = "#414350";
 static char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { normfgcolor, normbgcolor },
